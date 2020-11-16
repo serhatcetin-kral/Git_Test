b023e62bb35a590a5a831f1b6a275ad1ffcf385f
serhatcetin-kral <74295409+serhatcetin-kral@users.noreply.github.com>
2020-11-16 11:04:51 -0800

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,9 +3,8 @@ public class TestClass {
 
 
 public static  void main(String[] args){
-
-
-    System.out.println("hi git up deneme");
+ System.out.println("hi git up deneme");
     System.out.println("second ersion");
+     System.out.println("third  version");
 }
 }
